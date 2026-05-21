ec4d74658463efed9d48bfd9345efcdfc1cc79d0
vovatengu <47752512+vovatengu@users.noreply.github.com>
2026-05-21 18:58:15 +0700

    minifix

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -15,17 +15,17 @@ class Post
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $name = null;
+    private string $name = '';
 
     #[ORM\Column(type: Types::TEXT)]
-    private ?string $body = null;
+    private string $body = '';
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getName(): string
     {
         return $this->name;
     }
@@ -37,7 +37,7 @@ class Post
         return $this;
     }
 
-    public function getBody(): ?string
+    public function getBody(): string
     {
         return $this->body;
     }
