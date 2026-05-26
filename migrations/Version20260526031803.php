2cb6841a7696b0d0f32cf171deefe893c07675e1
vovatengu <47752512+vovatengu@users.noreply.github.com>
2026-05-26 10:52:11 +0700

    add migrations

---
+++ migrations/Version20260526031803.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260526031803 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `sonata_user` ADD COLUMN `gender` ENUM(\'male\', \'female\') DEFAULT NULL, ADD COLUMN `full_name` VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `sonata_user` DROP COLUMN `gender`, DROP COLUMN `full_name`');
+    }
+}
--- src/Entity/SonataUserUser.php
+++ src/Entity/SonataUserUser.php
@@ -7,12 +7,48 @@ use Sonata\UserBundle\Entity\BaseUser;
 
 // or `Sonata\UserBundle\Entity\BaseUser3` as BaseUser if you upgrade to doctrine/orm ^3
 
+enum GenderEnum: string
+{
+    case male = 'male';
+    case female = 'female';
+}
+
 #[ORM\Entity]
-#[ORM\Table(name: 'user__user')]
+#[ORM\Table(name: 'sonata_user')]
 class SonataUserUser extends BaseUser
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     protected $id;
+
+    #[ORM\Column(length: 255)]
+    private ?string $full_name = null;
+
+    #[ORM\Column(enumType: GenderEnum::class)]
+    private ?GenderEnum $gender = null;
+
+    public function getFullName(): ?string
+    {
+        return $this->full_name;
+    }
+
+    public function setFullName(?string $full_name): static
+    {
+        $this->full_name = $full_name;
+
+        return $this;
+    }
+
+    public function getGender(): ?GenderEnum
+    {
+        return $this->gender;
+    }
+
+    public function setGender(?GenderEnum $gender): static
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
 }
