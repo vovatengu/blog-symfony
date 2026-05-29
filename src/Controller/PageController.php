ed27ff517a3cfd3d98ebe43836fb56de9d746060
vovatengu <47752512+vovatengu@users.noreply.github.com>
2026-05-29 11:06:13 +0700

    add counters of session and cookie

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -4,17 +4,30 @@ namespace App\Controller;
 
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PageController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(PostRepository $postRepository): Response
+    public function index(Request $request, PostRepository $postRepository): Response
     {
-        return $this->render('page/index.html.twig', [
+        $sessionCounter = (int) $request->getSession()->get('session_counter', 0) + 1;
+        $request->getSession()->set('session_counter', $sessionCounter);
+
+        $cookieCounter = (int) $request->cookies->get('cookie_counter', 0) + 1;
+
+        $response = $this->render('page/index.html.twig', [
             'posts' => $postRepository->findAllWithAuthors(),
         ]);
+
+        $response->headers->setCookie(
+            Cookie::create('cookie_counter', (string) $cookieCounter, time() + (365 * 24 * 60 * 60))
+        );
+
+        return $response;
     }
 
     #[Route('/about', name: 'app_about')]
